639ad8a497a4345fe938da9a3e60880640c860d6
cappochinho <edwardprempeh18@gmail.com>
2023-01-21 16:09:28 +0000

    Added task #4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,28 @@
     The landing page of the web app
 """
 
+from flask import jsonify
 from api.v1.views import app_views
-import json
+from models import storage
+
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns a JSON"""
 
-    return json.dumps({"status":"OK"})
+    return jsonify({"status":"OK"})
+
+
+@app_views.route('/api/vi/stats', methods=['GET'])
+def stats():
+    """Displays a JSON"""
+
+    objects = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(objects)
